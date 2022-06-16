7cfac3955e03eebfa95b02da4617d1a65332f6b8
Mabure <107389077+Mabure@users.noreply.github.com>
2022-06-15 18:34:09 -0600

    Create Almacen.cpp

---
+++ Almacen.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int koy = 1;
+	int pos, caco, prod[10][2];	bool PExistir[10];
+
+
+		do{
+	/*GOTO SELECTPOS*/		SelectPos:
+				
+			printf("Seleccione la posicion donde se encontrara su producto (0-9) \n");
+			scanf("%i", &pos);
+			
+			if(PExistir[pos] == false){
+	/*GOTO SUS*/			SUS:
+				
+				printf("Cuanto valdra su producto?:  ");
+				scanf("%i", &caco);
+				
+				prod[pos][0] = caco;
+				
+				printf("Cuanto de este producto tiene?:  ");
+				scanf("%i", &caco);
+				
+				prod[pos][1] = caco;
+				
+				PExistir[pos] = true;
+			}
+			else{
+				printf("Esta posicion esta siendo ocupada por otro producto, Decea sustituarla?\n 	seleccione 1 para sustituir o  0 para escoger otra posicion\n ");
+				scanf("%i", &koy);
+			}
+			if(koy == 0){
+				goto SelectPos;
+			}
+			else if(koy == 1){
+				goto SUS;
+			}
+			
+			printf("seleccione 1 para continuar o 0 para salir\n");
+			scanf("%i", &koy);
+			
+		}while(koy != 0);
+		
+	return 0;
+}
